c3bade7903686169eb3964323b04a6051dc885e9
zmoon92 <zmoon92@gmail.com>
2020-08-25 11:49:06 -0400

    Add ms1
    starting examination of the given lat/lon grid and how it should
    translate into an x/y grid

---
+++ ms1/README.md
@@ -0,0 +1,2 @@
+
+[Task list (Canvas)](https://psu.instructure.com/courses/2054855/pages/ms-1-task-list?module_item_id=29450753)
---
+++ ms1/ms1.py
@@ -0,0 +1,70 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.5.2
+#   kernelspec:
+#     display_name: Python [conda env:meteo473_fa20]
+#     language: python
+#     name: conda-env-meteo473_fa20-py
+# ---
+# %%
+import matplotlib.pyplot as plt
+import numpy as np
+import xarray as xr
+
+# %% [markdown]
+# Load the data.
+
+# %%
+ds = xr.open_dataset("../data/data.nc")
+ds
+
+# %% [markdown]
+# > All figures for this and subsequent Milestones should have the axes labeled in physical units, i.e. kilometers (grid spacing is 3 km)
+#
+# Let's see what the km grid spacing actually would be (since this is a lat/lon grid, 3 km is presumably just an estimate).
+#
+# Note that we did check when creating the nc file that XLONG and XLAT only varied in one dimension.
+# * lon only varies in column (2nd) dimension
+#   e.g. `lon[0,:] - lon[-1,:]` is all zeros
+#
+# * lat only varies in row (1st) dimension
+#   e.g. `lat[:,0] - lat[:,-1]` is all zeros
+
+# %%
+lat = ds.lat.values
+lon = ds.lon.values
+
+# equal spacing?
+dlat = np.diff(lat)
+dlon = np.diff(lon)
+
+try:
+    assert np.all(dlat == dlat[0])
+    assert np.all(dlon == dlon[0])
+except AssertionError:
+    print("spacing is not equal...")
+
+
+latc = lat[:-1] + 0.5 * dlat
+lonc = lon[:-1] + 0.5 * dlon
+
+fig, [ax1, ax2] = plt.subplots(2, 1, figsize=(9, 6))
+
+ax1.plot(latc, dlat)
+ax1.set_xlabel("lat (deg.)")
+ax1.set_ylabel(r"$\Delta$lat (deg.)")
+
+ax2.plot(lonc, dlon)
+ax2.set_xlabel("lon (deg.)")
+ax2.set_ylabel(r"$\Delta$lon (deg.)")
+
+fig.tight_layout()
+
+# %%
+Lon, Lat = np.meshgrid(lon, lat)
